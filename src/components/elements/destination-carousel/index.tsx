abbe3ea5b4dbd5595a420de73cfa6461cb949fec
lusivelin <linhtetpaing.dev@gmail.com>
2024-10-09 15:09:54 +0700

    feat: add destination carousel

--- src/components/elements/destination-carousel/index.tsx
+++ src/components/elements/destination-carousel/index.tsx
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { SwiperOptions } from 'swiper/types';
+
+import { Navigation } from 'swiper/modules';
 
 const slides = [
   'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -9,48 +12,49 @@ const slides = [
   'https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
 ];
 
-const BannerCarousel: React.FC = () => {
+const destinationSwiperSetting: SwiperOptions = {
+  modules: [Navigation],
+  navigation: true,
+  slidesPerView: 4.4,
+  centeredSlides: true,
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 3.4
+    },
+    480: {
+      slidesPerView: 4.4
+    }
+  },
+  spaceBetween: 30
+}
+
+const DestinationCarousel: React.FC = () => {
   return (
-    <Swiper
-      slidesPerView={2.5}
-      loopAdditionalSlides={1}
-      initialSlide={2}
-      loop={true}
-      breakpoints={{
-        // 320: {
-        //   slidesPerView: 1,
-        //   spaceBetween: 10,
-        // },
-        // 768: {
-        //   slidesPerView: 2,
-        //   spaceBetween: 20,
-        // },
-        // 1024: {
-        //   slidesPerView: 2.4,
-        //   centeredSlides: true,
-        //   spaceBetween: 20,
-        // },
-      }}
-      spaceBetween={20}
-    >
-    {slides.map((slide, index) => (
-      <SwiperSlide key={index}>
-        <div
-          style={{
-            backgroundImage: `url(${slide})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            width: "100%",
-            height: "600px",
-            borderRadius: "10px",
-          }}
-        />
-      </SwiperSlide>
-    ))}
-  </Swiper>
+    <div className="lago-destination-carousel-wrapper">
+      <Swiper
+        {...destinationSwiperSetting}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                backgroundImage: `url(${slide})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                width: "100%",
+                height: "18vw",
+                borderRadius: "10px",
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
-export default BannerCarousel;
+export default DestinationCarousel;
--- src/components/pages/home/section-two/index.tsx
+++ src/components/pages/home/section-two/index.tsx
@@ -1,9 +1,15 @@
 import BannerCarousel from "@/components/elements/banner-carousel";
+import DestinationCarousel from "@/components/elements/destination-carousel";
 
 function HomeSectionTwo() {
   return (
     <section id="home-section-two">
       <BannerCarousel />
+
+      <br />
+      <br />
+
+      <DestinationCarousel />
     </section>
   );
 }
