3500abe937e9c00b56dfe8f3a8cc236f41c6f7bb
lusivelin <linhtetpaing.dev@gmail.com>
2024-10-22 10:48:12 +0700

    feat: setup sanity

---
+++ src/sanity/env.ts
@@ -0,0 +1,20 @@
+export const apiVersion =
+  process.env.SANITY_STUDIO_SANITY_API_VERSION || '2024-10-21'
+
+export const dataset = assertValue(
+  process.env.SANITY_STUDIO_SANITY_DATASET,
+  'Missing environment variable: SANITY_STUDIO_SANITY_DATASET'
+)
+
+export const projectId = assertValue(
+  process.env.SANITY_STUDIO_SANITY_PROJECT_ID,
+  'Missing environment variable: SANITY_STUDIO_SANITY_PROJECT_ID'
+)
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
