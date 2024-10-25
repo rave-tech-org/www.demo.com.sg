abbd132edca0077bff5a5efb8ccb48196e7f8a1d
lusivelin <linhtetpaing.dev@gmail.com>
2024-10-25 12:34:26 +0700

    chore: debugging-004 sanity env issue on deployment

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,13 +2,11 @@
  * This configuration file lets you run `$ sanity [command]` in this folder
  * Go to https://www.sanity.io/docs/cli to learn more.
  **/
-import * as dotenv from 'dotenv'
-
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 import { defineCliConfig } from 'sanity/cli';
 
-const projectId = process.env.SANITY_STUDIO_SANITY_PROJECT_ID;
-const dataset = process.env.SANITY_STUDIO_SANITY_DATASET;
-
-export default defineCliConfig({ api: { projectId, dataset } });
+export default defineCliConfig({
+  api: { projectId: "oytcbhek", dataset: "production" },
+});
