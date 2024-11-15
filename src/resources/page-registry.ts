af59c31aba6938c35e897c58a3a43fc92e2b4833
lusivelin <dev@rave.tech>
2024-11-15 14:02:34 +0700

    feat: add tours on page registry

--- src/resources/page-registry.ts
+++ src/resources/page-registry.ts
@@ -2,6 +2,7 @@ const pageRegistry = new Map([
   ['home-page', '/'],
   ['header-layout', '/content-block/navigation-menu'],
   ['footer-layout', '/content-block/navigation-menu'],
+  ['tour-search-page', '/tours'],
 ]);
 
 export default pageRegistry;
